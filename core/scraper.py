7f98a33491a96a2c2179d863afa22f3f9d14bbe9
کوروش غنی زاده <k.ghanizadeh@ava.com>
2022-02-16 11:37:51 +0330

    updated scraper for pair exchanges

--- core/scraper.py
+++ core/scraper.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import time
+
 import requests
 
 currency_codes = {
@@ -49,20 +51,51 @@ def get_currencies(base_url="https://www.tgju.org/currency"):
             for row in rows:
                 try:
                     title = row.th.text
-                    title = currency_codes[title]
-                    price = row.find("td", attrs={"class": "nf"})
-                    price = int("".join(price.text.split(",")))
-                    result[title] = price
-                except Exception as e:
+                    if title in currency_codes:
+                        title = currency_codes[title]
+                        price = row.find("td", attrs={"class": "nf"})
+                        price = int("".join(price.text.split(",")))
+                        result[title] = price
 
+                except Exception as e:
+                    print(e)
                     return False
         return result
     else:
         return False
 
 
+def get_pair_rate_manualy(base, target):
+    if base.lower() == "irr" or target.lower() == "irr":
+        exchanges = get_currencies()
+        if base.lower() == "irr":
+            return "{:.6f}".format(round(1 / exchanges[target], 6))
+        else:
+            return "{:.6f}".format(round(exchanges[base], 6))
+    else:
+        res = requests.get(f"https://www.tgju.org/diff")
+        soup = BeautifulSoup(res.content, "lxml")
+        table = soup.find("table", attrs={"id": "diff-table"})
+        trs = table.find_all("tr")[1:]
+        exchanges = {}
+        for tr in trs:
+            title = tr.find("th").text.split()[0]
+            rate = tr.find("td").text
+            exchanges[title] = rate
+        return "{:.6f}".format(float(exchanges[f"{base}/{target}"]))
+
+
+def get_pair_rate_automaticaly(base, target):
+    res = requests.get(f"https://www.tgju.org/diff")
+
+
 def main():
-    return print(get_currencies())
+    # return print(get_currencies())
+    print(get_pair_rate_manualy("usd", "eur"))
+    print(get_pair_rate_manualy("irr", "eur"))
+    print(get_pair_rate_manualy("eur", "irr"))
+    # print(get_pair_rate_manualy("sek", "usd"))
+    # print(get_pair_rate_manualy("kwd", "usd"))
 
 
 if __name__ == "__main__":
